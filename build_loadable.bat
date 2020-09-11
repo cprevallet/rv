107f1a44bdd1026411c4385e20a5ef7344222d31
Craig Prevallet <penguintx@hotmail.com>
2020-09-11 09:35:04 -0500

    Create utility scripts.

---
+++ build_loadable.bat
@@ -0,0 +1,1 @@
+go build -o .\appdlls\goroutines.dll -buildmode=c-shared .\cmd\goroutines.go .\cmd\wrappers.go
---
+++ build_loadable.sh
@@ -0,0 +1,1 @@
+go build -o ./appdlls/goroutines.so -buildmode=c-shared ./cmd/goroutines.go ./cmd/wrappers.go
