94c628fb2b57ac14a0582c03e607f2c660f40392
unknown <Craig>
2020-09-23 16:17:43 -0500

    Copy over the blt files after initial compile.

--- build_loadable.bat
+++ build_loadable.bat
@@ -5,3 +5,5 @@ mkdir .\interp\lib\fit1.0
 copy .\lib\libfit.dll .\interp\lib\fit1.0\libfit.dll
 copy .\lib\pkgIndex.tcl .\interp\lib\fit1.0\pkgIndex.tcl
 copy .\lib\fit.tcl .\interp\lib\fit1.0\fit.tcl
+mkdir .\interp\lib\tkblt3.2
+copy .\tkblt3.2\*.* .\interp\lib\tkblt3.2\*.*
