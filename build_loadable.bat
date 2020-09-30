2e4012ba358ac8d5e441b0e6b54418847fd2f327
Craig Prevallet <penguintx@hotmail.com>
2020-09-30 16:06:00 -0500

    Remove redundant batch file.  Makefile now handles this.

--- build_loadable.bat
+++
@@ -1,9 +0,0 @@
-REM Build
-go build -o .\lib\libfit.dll -buildmode=c-shared .\cmd\goroutines.go .\cmd\wrappers.go
-REM Install
-mkdir .\interp\lib\fit1.0
-copy .\lib\libfit.dll .\interp\lib\fit1.0\libfit.dll
-copy .\lib\pkgIndex.tcl .\interp\lib\fit1.0\pkgIndex.tcl
-copy .\lib\fit.tcl .\interp\lib\fit1.0\fit.tcl
-mkdir .\interp\lib\tkblt3.2
-copy .\tkblt3.2\*.* .\interp\lib\tkblt3.2\*.*
